d6d1d489ab726b17ae88686236b4c9d2c7c1a5a7
alkinuradia <112646930+alkinuradia@users.noreply.github.com>
2024-03-27 22:40:52 +0200

    home_work_calculator

--- HomeWork/js/script.js
+++ HomeWork/js/script.js
@@ -23,40 +23,68 @@
 //   const ivan = new Abonent("Ivan", "+380674558988");
 //   console.log(ivan); //{name: 'Ivan', telephone: '+380674558988'}
 
-class Abonent {
-  constructor(props) {
-    this.name = props.name;
-    this.tel = props.tel;
+// class Abonent {
+//   constructor(props) {
+//     this.name = props.name;
+//     this.tel = props.tel;
+//   }
+
+//   name = "";
+//   tel = "";
+
+//   set name(newName) {
+//     this.name = newName;
+//   }
+
+//   set tel(newTel) {
+//     this.tel = newTel;
+//   }
+//   getAbonent() {
+//     return ` ${this.name} + ${this.tel}`;
+//   }
+// }
+
+// const Kris = new Abonent({
+//   name: "Kristina",
+//   tel: "+380509682613",
+// });
+
+// const Dasha = new Abonent({
+//   name: "Dasha",
+//   tel: "+3863515111561",
+// });
+
+// const Ivan = new Abonent({
+//   name: "Ivan",
+//   tel: "+3806851515151",
+// });
+
+// console.log(Ivan);
+
+
+// Створіть функцію-конструктор Calculator, який створює об’єкти з трьома
+// методами:
+// read() запитує два значення за допомогою prompt і запам’ятовує їх у
+// властивостях об’єкта.
+// sum() повертає суму цих властивостей.
+// mul() повертає результат множення даних властивостей.
+
+function Calculator() {
+  this.read = function() {
+    this.a = +prompt("a?", 0);
+    this.b = +prompt("b?", 0)
   }
-
-  name = "";
-  tel = "";
-
-  set name(newName) {
-    this.name = newName;
+  this.sum = function() {
+    return this.a + this.b;
   }
-
-  set tel(newTel) {
-    this.tel = newTel;
+  this.mul = function() {
+    return this.a * this.b;
   }
-  getAbonent() {
-    return ` ${this.name} + ${this.tel}`;
-  }
-}
+};
 
-const Kris = new Abonent({
-  name: "Kristina",
-  tel: "+380509682613",
-});
 
-const Dasha = new Abonent({
-  name: "Dasha",
-  tel: "+3863515111561",
-});
+let calculator = new Calculator();
+calculator.read();
 
-const Ivan = new Abonent({
-  name: "Ivan",
-  tel: "+3806851515151",
-});
-
-console.log(Ivan);
+alert( "Sum=" + calculator.sum() );
+alert( "Mul=" + calculator.mul() );
